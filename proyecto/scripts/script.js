40c2d80e18be2957ee2e2d0b20cdb6ea3b01363b
Monterrosa23 <jose.monterrosa25@itca.edu.sv>
2025-05-13 22:13:51 -0600

    cambios grandes

---
+++ proyecto/scripts/script.js
@@ -0,0 +1,27 @@
+
+let indice = 0;
+const diapositivas = document.getElementById("diapositivas");
+const total = diapositivas.children.length;
+
+window.onscroll = function () {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (scrollTop / scrollHeight) * 100;
+    document.getElementById("scroll-progress").style.width = scrolled + "%";
+
+};
+
+function menuBot() {
+    const nav = document.getElementById('menu');
+    nav.classList.toggle('active');
+
+}
+
+
+
+
+function moverCarrusel(direccion) {
+    indice = (indice + direccion + total) % total;
+    diapositivas.style.transform = `translateX(-${indice * 100}%)`;
+    
+}
